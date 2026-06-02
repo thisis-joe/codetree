84f8cddfd76ac08c0386cc4ebd710798bf31bb92
우동균 <96711842+thisis-joe@users.noreply.github.com>
2026-06-02 13:13:05 +0900

    [Passed] 반복 출력하기 (99ms, 9MB)

---
+++ trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        print(n);
+        return;
+    }
+    static void print(int n){
+        for(int i = 0 ; i < n ; i++){
+            System.out.println("12345^&*()_");
+        }   
+    }
+}
