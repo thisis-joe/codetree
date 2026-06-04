7d65b91dfe0e8e79fb3a9673397c3fa91121c253
우동균 <96711842+thisis-joe@users.noreply.github.com>
2026-06-04 13:53:59 +0900

    [Passed] 함수를 이용한 연속부분수열 여부 판단하기 (134ms, 10MB)

--- trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,36 +1,46 @@
 import java.util.Scanner;
 public class Main {
+
+    static int n1,n2,a[],b[];
+
+    static boolean isSame(int n){
+        for(int s = 0 ; s < n2; s++){
+            if(a[s + n] != b[s]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isSubsequence(){
+        for(int i = 0 ; i <= n1-n2; i++){
+            if(isSame(i)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int[] a = new int[n1];
-        int[] b = new int[n2];
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        a = new int[n1];
+        b = new int[n2];
+        
         for (int i = 0; i < n1; i++)
             a[i] = sc.nextInt();
         for (int i = 0; i < n2; i++)
             b[i] = sc.nextInt();
         
-        //
-        for(int s = 0 ; s <= n1-n2; s++){
-
-            if(a[s] != b[0]) continue;
-
-            int bEndIdx = 0;
-            int cnt = 0;
-            for(int e=s; e<s+n2; e++){
-                if(a[e]!=b[bEndIdx++]) break;
-                else{
-                    cnt++;
-                }
-                if(cnt==n2 && a[e]==b[n2-1]){
-                    System.out.print("Yes");
-                    return;
-                }
-            
-            }
-        }
+       if(isSubsequence()){
+        System.out.print("Yes");
+        return;
+       }
+       else{
         System.out.print("No");
         return;
+       }
     }
 }
