f1cd220353696eba4c6493598606443a8e1260bc
우동균 <96711842+thisis-joe@users.noreply.github.com>
2026-06-04 17:32:55 +0900

    [Passed] 재귀함수의 꽃 (144ms, 11MB)

--- trail2/재귀함수의 꽃/the-flower-of-the-recursive-function.java
+++ trail2/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -1,31 +1,17 @@
 import java.util.Scanner;
 public class Main {
-    static int n;
-    static boolean flag;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        flag = true;
-
+        int n = sc.nextInt();
         print(n);
     }
 
     static void print(int cnt){
-        if(cnt==1 && flag == true){
-            System.out.print(cnt+" ");
-            flag = false;
-        }
-        if(cnt==n+1 && flag == false){
-            return;
-        }
-        if(flag){
-            System.out.print(cnt+" ");
-            print(cnt-1);
-        }
-        else{
-            System.out.print(cnt+" ");
-            print(cnt+1);
-        }
+        if(cnt==0) return;
 
+        System.out.print(cnt +" ");
+        print(cnt-1);
+        System.out.print(cnt +" ");
+        
     }
 }
