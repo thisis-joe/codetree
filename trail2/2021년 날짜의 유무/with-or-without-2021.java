d6a698ef5ba964262f32b0bc12f1d7c1b2a9d314
우동균 <96711842+thisis-joe@users.noreply.github.com>
2026-06-04 14:33:03 +0900

    [Passed] 2021년 날짜의 유무 (129ms, 10MB)

--- trail2/2021년 날짜의 유무/with-or-without-2021.java
+++ trail2/2021년 날짜의 유무/with-or-without-2021.java
@@ -1,35 +1,33 @@
 import java.util.Scanner;
-import java.io.*;
-import java.util.*;
 
-public class Main {
-    static int m,d;
-
-    static boolean isExists(int m,int d){
-        //
-        if(m<1 || m>12 || d<1 || d>31) return false;
-        if(m==2){
-            if(d>28) return false;
-        }
-        else if(m==4 || m==6 || m==9 || m==11){
-            if(d>30) return false;
-        }
+public class Main {    
+    // 윤년이 아닐 때 m번째 달의 마지막 날을 반환하는 함수를 작성합니다.
+    public static int lastDayNumber(int m) {
+        if(m == 2)
+            return 28;
+        if(m == 4 || m == 6 || m == 9 || m == 11)
+            return 30;
         
-        return true;
+        return 31;
+    }
+    
+    // 윤년이 아닐 때 m월 d일이 존재하는지 여부를 확인하는 함수를 작성합니다.
+    public static boolean judgeDay(int m, int d) {
+        if(m <= 12 && d <= lastDayNumber(m))
+            return true;
+        
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        m = sc.nextInt();
-        d = sc.nextInt();
+        // 변수 선언 및 입력:
+        int m = sc.nextInt();
+        int d = sc.nextInt();
         
-        if(isExists(m,d)){
+        if(judgeDay(m, d))
             System.out.print("Yes");
-            return;
-        }
-        else{
+        else
             System.out.print("No");
-            return;
-        }
     }
 }
