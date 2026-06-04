26882cd27a4ccae85d01ef9a984a999516b5ecc4
우동균 <96711842+thisis-joe@users.noreply.github.com>
2026-06-04 20:48:38 +0900

    [Passed] 재귀함수를 이용한 최댓값 (99ms, 9MB)

--- trail2/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
+++ trail2/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -10,17 +10,13 @@ public class Main {
         for(int i = 0 ; i < n ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        if(n==1) {
-            System.out.print(arr[0]);
-            return;
-        }
-        
+       
         System.out.print(recur(0)); 
         return;
     }
 
     static int recur(int idx){
-        if(idx==n-2) return (int)Math.max(arr[n-2],arr[n-1]);
+        if(idx==n-1) return arr[n-1];
         return (int)Math.max(arr[idx],recur(idx+1));
     }
 }
