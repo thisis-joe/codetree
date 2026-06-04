e81d644dd479050ab2cd844547683e73d7302a5f
우동균 <96711842+thisis-joe@users.noreply.github.com>
2026-06-04 20:25:15 +0900

    [Runtime Error] 재귀함수를 이용한 최댓값 (93ms, 9MB)

---
+++ trail2/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int n, arr[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.print(recur(0));
+        
+    }
+
+    static int recur(int idx){
+        if(idx==n-2) return (int)Math.max(arr[n-2],arr[n-1]);
+        return (int)Math.max(arr[idx],recur(idx+1));
+    }
+}
