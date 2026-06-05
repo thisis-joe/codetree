bfce2e4ea5c75a87a7b163fb81df4508f718ea99
우동균 <96711842+thisis-joe@users.noreply.github.com>
2026-06-05 15:25:58 +0900

    [Wrong Answer] 재귀함수를 이용한 최소공배수 (88ms, 9MB)

--- trail2/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ trail2/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -14,12 +14,9 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int gcd = 0;
         int lcm = 0;
-        int multi = arr[0];
         for(int i = 1; i < n ; i++){
-            gcd = gcd(arr[i-1],arr[i]);
-            lcm = arr[i-1] * arr[i] / gcd;
+            lcm = arr[i-1] * arr[i] / gcd(arr[i-1],arr[i]);
             arr[i] = lcm;
         }
 
@@ -28,7 +25,6 @@ public class Main {
     }
 
     static int gcd(int a, int b){
-        // if(a%b==0) return b;
         if(b==0) return a;
         return gcd(b,a%b);
     }
